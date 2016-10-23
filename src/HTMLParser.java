7fd8e6d91be4b534f4093d7e5627c221326b9f3f
Mark093 <marco.lm993@gmail.com>
2016-10-23 15:10:57 +0200

    Steps forward in the HTMLParser class

--- src/HTMLParser.java
+++ src/HTMLParser.java
@@ -1,6 +1,6 @@
 /**
  * Created by marco on 21/10/16.
- * In this class we will read the weboage related to the given word and we will extract data from it
+ * In this class we will read the webpage related to the given word and we will extract data from it
  */
 
 import org.jsoup.Jsoup;
@@ -33,7 +33,30 @@ public class HTMLParser {
 
     //Parser for the single table
     static void ParseTable(Element table) {
+        //TODO: find a way to detect if a table is interesting or not for us (maybe when we couldn't detect language)
+        String lang=getLanguage(table);
+        if (lang.isEmpty()) System.out.println("Language not found");
+        else System.out.println(lang);
+    }
 
+    //Get the language of the table content
+    static String getLanguage(Element table) {
+        boolean found=false;
+        String lang = new String("");
+        //Get the node at the same level of the XML tag with the language
+        Element divFrame = table.parent().parent();
+        //Search the node containing the language
+        while (!found && divFrame!=null) {
+            if (divFrame.tagName().equalsIgnoreCase("h2")) {
+                Element langel=divFrame.child(0);
+                if (langel.className().equalsIgnoreCase("mw-headline")) {
+                    lang = langel.text();
+                    found=true;
+                }
+            }
+            divFrame = divFrame.previousElementSibling();
+        }
+        return lang;
     }
 
 }
