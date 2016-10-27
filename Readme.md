02c9a0c41eb7ecabc45e384d2dde6b06053d57ef
Mark093 <marco.lm993@gmail.com>
2016-10-27 21:37:12 +0200

    HTMLParser improved: now it considers also parenthetical lists and prints only the POS listed in allowedPOS. POS detection improved

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@ Until now there are three classes:
     -Main: it contains the main function that loads the xml dump of Wiktionary and starts parsing it by using the TitlesHandler class described below.
     -TitlesHandler: it's an extension of the SAX default handler. It parses only the "title" tags in the XML file and calls HTMLParser on it.
     -HTMLParser: here we download the HTML pages related to the titles parsed before and we get all the info from it.
+    -HeaderList: by using a txt file it gets the tables from few pages of wiktionary and use them to get the names of the headers of the tables.
+    -PrepTable: it is used to preprocess the table: it gives the main info of it and the list of the cells contained in it (of type Word).
+    -Word: A particular data structure: contains the single word and all the headers related to it.
+    -WordHeaders: It's a particular data structure for representing an header of a word.
--- src/HeaderList.java
+++ src/HeaderList.java
@@ -1,11 +1,56 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by marco on 27/10/16.
  * This class has to get te file and use this to compute all the headers names (frequency of the word in tables check)
  */
 public class HeaderList {
+    //'frequence' is the min number of times that a word has to appear in order to be considered header.
+    private final int frequence=5;
+    private Map<String, Integer> headers;
 
-    public HeaderList(String file) {
+    public HeaderList(String nfile) {
+        BufferedReader reader;
+        try {
+            String currline;
+            reader = new BufferedReader(new FileReader(nfile));
+            while((currline=reader.readLine()) != null) {
+                headers=addCells(headers,frequentCells(currline));
+            }
+        }
+        catch (IOException e) {}
+    }
 
+    public Map<String, Integer> getHeaders() {
+        return new HashMap<>(headers);
+    }
+
+    private static Map<String, Integer> frequentCells(String page){
+        Map<String, Integer> retvalue=new HashMap<>();
+        //TODO: parse cells and count them (hint: remove whitespaces at the beginning and the end of the string by using trim() and use frequence as threshold)
+        try {
+            //Open the Wiktionary page
+            Document doc = Jsoup.connect("https://en.wiktionary.org/wiki/" + page).get();
+            //Get all the tables
+            Elements tables= doc.getElementsByTag("table");
+            //Extract the words from the cells and check them
+            //TODO
+        }
+        catch (IOException e) {}
+        return retvalue;
+    }
+
+    private static Map<String, Integer> addCells(Map<String, Integer> initial, Map<String, Integer> toadd) {
+        //TODO: add rows if the cell doesn't exists, otherwise add counter.
+        return initial;
     }
 
 }
